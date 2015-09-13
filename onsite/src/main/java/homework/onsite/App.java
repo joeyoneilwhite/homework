b146aaabf383c078421e1a7c6a9460a8813097f9
joeyoneilwhite <joey.white@ihealthtechnologies.com>
2015-09-12 21:49:04 -0400

    multiply numbers

---
+++ onsite/src/main/java/homework/onsite/App.java
@@ -0,0 +1,13 @@
+package homework.onsite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
